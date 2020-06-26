ab1c93c6ab01296e4ee8fd568317d37b8f80c7b0
Cornholius <y.layshkin@gmail.com>
2020-06-26 22:36:41 +0300

    0.20.0626.2236
    сделана корзина, полирнул внешку

--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,7 @@ from django.views import View
 from .models import Food
 from cart.forms import CartAddProductForm
 
-
+color = ('994848', '48994b', '7f4899', '486e99', 'a63223', '9ba623', '23a66f', '948e8f', )
 class MainPage(View):
 
     def get(self, request):
@@ -33,7 +33,7 @@ class TodayInMenu(View):
 class AllMenu(View):
 
     def get(self, request):
-        print(request.get)
         menu_list = Food.objects.all()
         cart_form = CartAddProductForm
         return render(request, 'main/menulist.html', {'form': menu_list, 'cart_product_form': cart_form})
+
