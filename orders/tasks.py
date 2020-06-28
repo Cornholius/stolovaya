dc1e70d8fb7f420f6a4272081aa3713853024559
Cornholius <y.layshkin@gmail.com>
2020-06-29 00:46:39 +0300

    0.20.0629.0044
    добавлено оформление заказа на почту. чисто технически работает, но требует полировки

---
+++ orders/tasks.py
@@ -0,0 +1,22 @@
+from celery import task
+from django.core.mail import send_mail
+from .models import Order
+
+
+@task
+def order_created(order_id):
+    """
+    Задача для отправки уведомления по электронной почте при успешном создании заказа.
+    """
+    order = Order.objects.get(id=order_id)
+    subject = 'Ваш заказ на сайте <...> успешно оформлен'.format(order_id)
+    message = 'Здравствуй {},\n' \
+              'Тебе хватило мозгов оформить заказ, поздравляю!\n' \
+              'Твой заказ под номером {} ждёт тебя.\n ' \
+              'Приди и забери свой хавчик.'.format(order.first_name,
+                                             order.id)
+    mail_sent = send_mail(subject,
+                          message,
+                          'yar.corn@yandex.ru',
+                          [order.email])
+    return mail_sent
