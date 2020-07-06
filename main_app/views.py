d13a6c954a91802e4b7128e943704de25b23d5d2
Cornholius <y.layshkin@gmail.com>
2020-07-06 20:00:01 +0300

    0.20.0707.1959
    переделываб=ю внешку

--- main_app/views.py
+++ main_app/views.py
@@ -3,10 +3,13 @@ from django.views import View
 from .models import Food
 from cart.forms import CartAddProductForm
 
+
 color = ('994848', '48994b', '7f4899', '486e99', 'a63223', '9ba623', '23a66f', '948e8f', )
 class MainPage(View):
 
     def get(self, request):
+        q = request.session
+        print(request.META['OS'])
         menu_list = Food.objects.all()
         cart_form = CartAddProductForm
         return render(request, 'main/menulist.html', {'form': menu_list, 'cart_product_form': cart_form})
