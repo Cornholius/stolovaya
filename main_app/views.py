f73aede0402816af878fc30d26669ec7058f6cf3
Яр <y.layshkin@gmail.com>
2020-07-08 15:15:46 +0300

    20.0708.1514
    категории вынесены в отдельные модели, навёл небольшой порядок в админке

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-from .models import Food
+from .models import Food, Category
 from cart.forms import CartAddProductForm
 
 
@@ -19,7 +19,8 @@ class Menu(View):
 
     def get(self, request, menu_type=None):
         if menu_type:
-            menu_list = Food.objects.filter(type=menu_type)
+            category = Category.objects.get(slug=menu_type).id
+            menu_list = Food.objects.filter(category_id=category)
             cart_form = CartAddProductForm
             return render(request, 'main/menulist.html', {'form': menu_list, 'cart_product_form': cart_form})
 
@@ -27,6 +28,6 @@ class Menu(View):
 class TodayInMenu(View):
 
     def get(self, request):
-        menu_list = Food.objects.filter(type='salads')
+        menu_list = Food.objects.filter(category_id='1')
         cart_form = CartAddProductForm
         return render(request, 'main/today.html', {'form': menu_list, 'cart_product_form': cart_form})
