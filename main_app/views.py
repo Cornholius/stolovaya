3c6d75e2be55a760e863b1454f798ec21373dc09
Cornholius <y.layshkin@gmail.com>
2020-08-01 20:14:20 +0300

    20.0801.2014
    промежуточный

--- main_app/views.py
+++ main_app/views.py
@@ -15,12 +15,14 @@ class MainPage(TemplateView):
 
     def get(self, request):
         week = Weeks.objects.get(id=1)
-        print(week.days.get(day='Понедельник').food.all())
+        print(week.days.get(day='Завтраки').food.all())
         return render(request, 'main/mainpage.html', {'Monday': week.days.get(day='Понедельник').food.all(),
                                                       'Tuesday': week.days.get(day='Вторник').food.all(),
                                                       'Wednesday': week.days.get(day='Среда').food.all(),
                                                       'Thursday': week.days.get(day='Четверг').food.all(),
-                                                      'Friday': week.days.get(day='Пятница').food.all()})
+                                                      'Friday': week.days.get(day='Пятница').food.all(),
+                                                      'Breakfast': week.days.get(day='Завтраки').food.all(),
+                                                      'Drinks': week.days.get(day='Напитки').food.all()})
 
     def get_context_data(self, **kwargs):
         context = super(MainPage, self).get_context_data(**kwargs)
